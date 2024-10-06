8f4a5250005364e62ce5f70c38c3d4e15e260af5
Angelie Borromeo <angelieqborromeo@gmail.com>
2024-10-06 21:30:36 +0800

    cleanup for the ignore files

--- build/chairbot_localization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aqborromeo/chairbot_ws/src/chairbot_localization
-# Build directory: /home/aqborromeo/chairbot_ws/build/chairbot_localization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/chairbot_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aqborromeo/chairbot_ws/src/chairbot_simulation
-# Build directory: /home/aqborromeo/chairbot_ws/build/chairbot_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
